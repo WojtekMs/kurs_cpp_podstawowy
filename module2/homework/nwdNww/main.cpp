d8d5a5bb0c58e03f140f0e7a0c1d516b16a71240
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-05-27 01:16:53 +0200

    tasks nr 2 & 4

--- module2/homework/nwdNww/main.cpp
+++ module2/homework/nwdNww/main.cpp
@@ -4,6 +4,5 @@
 int main() {
   std::cout << "NWW(255, 15) = " << NWW(255, 15) << "\n";
   std::cout << "NWD(255, 15) = " << NWD(255, 15) << "\n";
-
   return 0;
 }
--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,31 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs == 0) {
+        return rhs;
+    }
+    if (rhs == 0) {
+        return lhs;
+    }
+    if (lhs < rhs) {
+        std::swap(lhs,rhs);
+    }
+    while(lhs % rhs != 0) {
+        int temp = lhs;
+        lhs = rhs;
+        rhs = temp % rhs;
+    }
+    return (rhs > 0) ? rhs: -rhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (NWD(lhs, rhs) != 0) {
+        if (lhs * rhs > 0) {
+            return (lhs * rhs)/(NWD(lhs, rhs));
+        }
+        return -(lhs * rhs)/(NWD(lhs, rhs));
+        
+    }
+    return 0;
+    
 }
